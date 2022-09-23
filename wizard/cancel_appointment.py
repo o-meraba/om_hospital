ff425190a5fa8f0b9dcdfbda82baf455cf0ecb19
omeraba <omeraba93@gmail.com>
2022-09-23 18:00:47 +0300

    overrided default get function

--- wizard/cancel_appointment.py
+++ wizard/cancel_appointment.py
@@ -1,3 +1,4 @@
+import datetime
 from odoo import api, fields, models
 
 
@@ -9,10 +10,12 @@ class CancelAppointmentWizard(models.TransientModel):
     def default_get(self, fields):
         res = super(CancelAppointmentWizard, self).default_get(fields)
         print("Default get executed", res)
+        res['date_cancel'] = datetime.date.today()
+        print("Default get executed 2", res)
         return res
 
     appointment_id = fields.Many2one('hospital.appointment', string="Appointment")
-    reason = fields.Text(string="Reason")
+    reason = fields.Text(string="Reason", default='Test Reason')
     date_cancel = fields.Date(string="Cancellation Date")
 
 
