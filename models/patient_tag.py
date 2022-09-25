5efe39165b18243c2c80cdf7f8e36ccc15ba8c11
omeraba <omeraba93@gmail.com>
2022-09-25 23:44:53 +0300

    added sql constraints

--- models/patient_tag.py
+++ models/patient_tag.py
@@ -9,4 +9,10 @@ class PatientTag(models.Model):
     active = fields.Boolean(string="Active", default=True)
     color = fields.Integer(string="Color")
     color_2 = fields.Char(string="Color 2")
+    sequence = fields.Integer(string="Sequence")
 
+    _sql_constraints = [
+        ('name_uniq', 'unique (name, active)', 'Tag name must be unique'),
+        ('check_sequence', 'CHECK(sequence > 0)', 'Sequence must be bigger than zero'),
+        ('seq_uniq', 'unique (sequence)', 'Sequence must be unique'),
+    ]
