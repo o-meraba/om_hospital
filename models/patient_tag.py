6e3536477e7a06685ed772d8227d979dd0179aa4
omeraba <omeraba93@gmail.com>
2022-09-26 18:38:29 +0300

    added copy=false attribute for fields

--- models/patient_tag.py
+++ models/patient_tag.py
@@ -6,9 +6,9 @@ class PatientTag(models.Model):
     _description = "Patient Tag"
 
     name = fields.Char(string='Name', required=True)
-    active = fields.Boolean(string="Active", default=True)
+    active = fields.Boolean(string="Active", default=True, copy=False)
     color = fields.Integer(string="Color")
-    color_2 = fields.Char(string="Color 2")
+    color_2 = fields.Char(string="Color 2", copy=False)
     sequence = fields.Integer(string="Sequence")
 
     @api.returns('self', lambda value: value.id)
