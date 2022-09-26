e9a1cbb80583d674b3bdd5f4386cd2b9cfc512a0
omeraba <omeraba93@gmail.com>
2022-09-26 12:36:59 +0300

    added 'copy function' to patient_tag copy attribute

--- models/patient_tag.py
+++ models/patient_tag.py
@@ -11,8 +11,21 @@ class PatientTag(models.Model):
     color_2 = fields.Char(string="Color 2")
     sequence = fields.Integer(string="Sequence")
 
+    @api.returns('self', lambda value: value.id)
+    def copy(self, default=None):
+        if default is None:
+            default = {}
+        if not default.get('name'):
+            default['name'] = self.name + " (Copy)"
+            default['sequence'] = self.sequence + 100
+
+        return super(PatientTag, self).copy(default)
+
+
+
     _sql_constraints = [
         ('name_uniq', 'unique (name, active)', 'Tag name must be unique'),
         ('check_sequence', 'CHECK(sequence > 0)', 'Sequence must be bigger than zero'),
         ('seq_uniq', 'unique (sequence)', 'Sequence must be unique'),
     ]
+
