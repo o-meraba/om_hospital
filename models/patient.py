0d3d094311e508dabb670fc109d60d3343232e52
omeraba <omeraba93@gmail.com>
2022-09-27 15:46:24 +0300

    made required attr based on conditions

--- models/patient.py
+++ models/patient.py
@@ -24,7 +24,7 @@ class HospitalPatient(models.Model):
     appointment_count = fields.Integer(string="Appointment Count", compute='_compute_appointment_count', store=True)
     appointment_ids = fields.One2many('hospital.appointment', 'patient_id', string="Appointments")
     parent = fields.Char(string="Parent")
-    marital_status = fields.Selection([('married','Married'), ('single', 'Single')], string="Marital Status", tracking=True)
+    marital_status = fields.Selection([('married', 'Married'), ('single', 'Single')], string="Marital Status", tracking=True)
     partner_name = fields.Char(string="Partner Name")
 
 
