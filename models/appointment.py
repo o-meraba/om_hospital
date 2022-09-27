a43cf94c6c7b67fc04f44a74a633c482a53f525f
omeraba <omeraba93@gmail.com>
2022-09-27 13:48:57 +0300

    added ondelete='cascade' feature

--- models/appointment.py
+++ models/appointment.py
@@ -9,7 +9,7 @@ class HospitalAppointment(models.Model):
     _description = "Hospital Appointment"
     _rec_name = "appointment_ref"
 
-    patient_id = fields.Many2one(comodel_name='hospital.patient', string="Patient", ondelete='restrict') # you can use just 'hospital.patient' but it should place first in parentheses
+    patient_id = fields.Many2one(comodel_name='hospital.patient', string="Patient", ondelete='cascade') # you can use just 'hospital.patient' but it should place first in parentheses
     appointment_time = fields.Datetime(string="Appointment Time", default=fields.Datetime.now)
     booking_date = fields.Date(string="Booking Date", default=fields.Date.today)
     gender = fields.Selection(related='patient_id.gender')
