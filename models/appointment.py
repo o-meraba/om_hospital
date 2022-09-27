d6c7fb358226289445bff87fc8befb604dbd6a98
omeraba <omeraba93@gmail.com>
2022-09-27 17:45:18 +0300

    added button in tree view header

--- models/appointment.py
+++ models/appointment.py
@@ -50,7 +50,8 @@ class HospitalAppointment(models.Model):
 
     def action_in_consultation(self):
         for rec in self:
-            rec.state = 'in_consultation'
+            if rec.state == 'draft':
+                rec.state = 'in_consultation'
 
     def action_done(self):
         for rec in self:
