55a35626e78c3ce973bea6337ec91e592749d607
omeraba <omeraba93@gmail.com>
2022-10-04 11:37:15 +0300

    added a server action as 'mark as done' for appointment

--- models/playground.py
+++ models/playground.py
@@ -5,8 +5,17 @@ class OdooPlayGround(models.Model):
     _name = "odoo.playground"
     _description = "Odoo PlayGround"
 
-    DEFAULT_ENV_VARIABLES = """# Available variables
-    # -self : Current Object"""
+    DEFAULT_ENV_VARIABLES = """# Available variables:
+#  - env: Odoo Environment on which the action is triggered
+#  - model: Odoo Model of the record on which the action is triggered; is a void recordset
+#  - record: record on which the action is triggered; may be void
+#  - records: recordset of all records on which the action is triggered in multi-mode; may be void
+#  - time, datetime, dateutil, timezone: useful Python libraries
+#  - float_compare: Odoo function to compare floats based on specific precisions
+#  - log: log(message, level='info'): logging function to record debug information in ir.logging table
+#  - UserError: Warning Exception to use with raise
+#  - Command: x2Many commands namespace
+# To return an action, assign: action = {...}"""
     model_id = fields.Many2one('ir.model', string='Model')
     code = fields.Text(string="Code")
     result = fields.Text(string='Result')
