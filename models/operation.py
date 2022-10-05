dd9968392b8da05b7556230e1ea92bd31f531459
omeraba <omeraba93@gmail.com>
2022-10-05 18:19:24 +0300

    added widget='handle' for drag and change records in TreeView

--- models/operation.py
+++ models/operation.py
@@ -4,11 +4,14 @@ class HospitalOperation(models.Model):
     _name = "hospital.operation"
     _description = "Hospital Operation"
     _log_access = False # Remove create_date, create_uid, write_date, write_uid columns
+    _order = 'sequence,id'
 
     doctor_id = fields.Many2one('res.users', string='Doctor')
     operation_name = fields.Char(string="Operation Name")
     reference_record = fields.Reference(selection=[('hospital.patient', 'Patient'),
                                                    ('hospital.appointment', 'Appointment')], string='Record')
+
+    sequence = fields.Integer(string="Sequence", default=10)
 
     @api.model
     def name_create(self, name):
