47266a61cfb7bc3ea3eb52a227988f751e1123aa
o-meraba <omeraba93@gmail.com>
2024-03-05 17:55:08 +0300

    added signup controllers

--- controllers/main.py
+++ controllers/main.py
@@ -1,5 +1,14 @@
 from odoo import http
 from odoo.http import request
+import logging
+import werkzeug
+
+from odoo import http, tools, _
+from odoo.addons.auth_signup.models.res_users import SignupError
+from odoo.addons.web.controllers.main import ensure_db, Home, SIGN_UP_REQUEST_PARAMS
+from odoo.addons.base_setup.controllers.main import BaseSetup
+from odoo.exceptions import UserError
+from odoo.http import request
 
 class Hospital(http.Controller):
     @http.route('/hospital/doctor/', website=True, auth='public')
@@ -9,3 +18,13 @@ class Hospital(http.Controller):
         return request.render("om_hospital.patients_page",{
             'patients': patients,
         })
+
+class AuthSignupHome(Home):
+
+    def _prepare_signup_values(self, qcontext):
+        values = { key: qcontext.get(key) for key in ('login', 'name', 'password') }
+       
+        if len(values.get('password')) < 8:
+            raise UserError(_('Şifre 8 karakterden kısa. Lütfen yeni bir şifre girin.Hospitalll'))
+           
+        return values 
